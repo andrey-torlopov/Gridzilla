af028aa73025b038038da1f60509083345737d00
Andrey Torlopov <torlopov.mail@ya.ru>
2025-10-25 10:05:51 +0300

    Initial Commit

---
+++ Gridzilla/GridzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridzillaApp.swift
+//  Gridzilla
+//
+//  Created by Andrey Torlopov on 25.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct GridzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GridzillaTests/GridzillaTests.swift
@@ -0,0 +1,17 @@
+//
+//  GridzillaTests.swift
+//  GridzillaTests
+//
+//  Created by Andrey Torlopov on 25.10.2025.
+//
+
+import Testing
+@testable import Gridzilla
+
+struct GridzillaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
